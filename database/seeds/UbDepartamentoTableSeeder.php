1691b8c5dd35281527fc3ea807625ce32294a484
vagrant <vagrant@homestead>
2018-09-30 18:46:23 +0000

    departamentos

--- database/seeds/UbDepartamentoTableSeeder.php
+++ database/seeds/UbDepartamentoTableSeeder.php
@@ -13,15 +13,15 @@ class UbDepartamentoTableSeeder extends Seeder
     public function run()
     {
         DB::table('ub_departamento')->insert([
-            ['nombre' => 'Lambayeque', 'slug' => str_slug('Departamento 1'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre' => 'La Libertad', 'slug' => str_slug('Departamento 2'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre' => 'Amazonas', 'slug' => str_slug('Departamento 3'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre' => 'Cajamarca', 'slug' => str_slug('Departamento 4'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre' => 'San Martin', 'slug' => str_slug('Departamento 5'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre' => 'Ancash', 'slug' => str_slug('Departamento 6'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre' => 'Tumbes', 'slug' => str_slug('Departamento 7'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre' => 'Piura', 'slug' => str_slug('Departamento 8'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
-            ['nombre' => 'Amazonas', 'slug' => str_slug('Departamento 9'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre' => 'Lambayeque', 'slug' => str_slug('Lambayeque'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre' => 'La Libertad', 'slug' => str_slug('La Libertad'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre' => 'Amazonas', 'slug' => str_slug('Amazonas'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre' => 'Cajamarca', 'slug' => str_slug('Cajamarca'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre' => 'San Martin', 'slug' => str_slug('San Martin'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre' => 'Ancash', 'slug' => str_slug('Ancash'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre' => 'Tumbes', 'slug' => str_slug('Tumbes'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre' => 'Piura', 'slug' => str_slug('Piura'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['nombre' => 'Amazonas', 'slug' => str_slug('Amazonas'), 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
         ]);
     }
 }
