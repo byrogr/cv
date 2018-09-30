4a69d5110d2d6791c18c1a30e0f52e89a25de33d
Roger Rojas <roger.rojas.effio@gmail.com>
2018-09-30 10:34:35 -0500

    cambios varios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,9 @@
     <!-- Favicon  -->
     <link rel="icon" href="img/core-img/favicon.ico">
 
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <!-- Core Style CSS -->
     <link href="{{ asset('css/core-style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
@@ -33,7 +36,7 @@
             <div class="col-12">
                 <div class="search-content">
                     <form action="#" method="get">
-                        <input type="search" name="search" id="search" placeholder="Type your keyword...">
+                        <input type="search" name="search" id="search" placeholder="Cual es su destino?">
                         <button type="submit"><img src="{{ asset('img/core-img/search.png') }}" alt=""></button>
                     </form>
                 </div>
@@ -50,7 +53,7 @@
     <div class="mobile-nav">
         <!-- Navbar Brand -->
         <div class="amado-navbar-brand">
-            <a href="index.html"><img src="{{ asset('img/core-img/logo.png') }}" alt=""></a>
+            <a href="{{ route('home') }}"><img src="{{ asset('img/core-img/logo3.png') }}" alt=""></a>
         </div>
         <!-- Navbar Toggler -->
         <div class="amado-navbar-toggler">
@@ -58,46 +61,7 @@
         </div>
     </div>
 
-    <!-- Header Area Start -->
-    <header class="header-area clearfix">
-        <!-- Close Icon -->
-        <div class="nav-close">
-            <i class="fa fa-close" aria-hidden="true"></i>
-        </div>
-        <!-- Logo -->
-        <div class="logo">
-            <a href="index.html"><img src="{{ asset('img/core-img/logo.png') }}" alt=""></a>
-        </div>
-        <!-- Amado Nav -->
-        <nav class="amado-nav">
-            <ul>
-                <li class="active"><a href="index.html">Home</a></li>
-                <li><a href="shop.html">Shop</a></li>
-                <li><a href="product-details.html">Product</a></li>
-                <li><a href="cart.html">Cart</a></li>
-                <li><a href="checkout.html">Checkout</a></li>
-            </ul>
-        </nav>
-        <!-- Button Group -->
-        <div class="amado-btn-group mt-30 mb-100">
-            <a href="#" class="btn amado-btn mb-15">%Discount%</a>
-            <a href="#" class="btn amado-btn active">New this week</a>
-        </div>
-        <!-- Cart Menu -->
-        <div class="cart-fav-search mb-100">
-            <a href="cart.html" class="cart-nav"><img src="{{ asset('img/core-img/cart.png') }}" alt=""> Cart <span>(0)</span></a>
-            <a href="#" class="fav-nav"><img src="{{ asset('img/core-img/favorites.png') }}" alt=""> Favourite</a>
-            <a href="#" class="search-nav"><img src="{{ asset('img/core-img/search.png') }}" alt=""> Search</a>
-        </div>
-        <!-- Social Button -->
-        <div class="social-info d-flex justify-content-between">
-            <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-        </div>
-    </header>
-    <!-- Header Area End -->
+    @include('layouts.header')
 
     <!-- Product Catagories Area Start -->
     @yield('content')
@@ -131,55 +95,7 @@
 <!-- ##### Newsletter Area End ##### -->
 
 <!-- ##### Footer Area Start ##### -->
-<footer class="footer_area clearfix">
-    <div class="container">
-        <div class="row align-items-center">
-            <!-- Single Widget Area -->
-            <div class="col-12 col-lg-4">
-                <div class="single_widget_area">
-                    <!-- Logo -->
-                    <div class="footer-logo mr-50">
-                        <a href="index.html"><img src="img/core-img/logo2.png" alt=""></a>
-                    </div>
-                    <!-- Copywrite Text -->
-                    <p class="copywrite"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-                </div>
-            </div>
-            <!-- Single Widget Area -->
-            <div class="col-12 col-lg-8">
-                <div class="single_widget_area">
-                    <!-- Footer Menu -->
-                    <div class="footer_menu">
-                        <nav class="navbar navbar-expand-lg justify-content-end">
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#footerNavContent" aria-controls="footerNavContent" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
-                            <div class="collapse navbar-collapse" id="footerNavContent">
-                                <ul class="navbar-nav ml-auto">
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="index.html">Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="shop.html">Shop</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="product-details.html">Product</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="cart.html">Cart</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="checkout.html">Checkout</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
+@include('layouts.footer')
 <!-- ##### Footer Area End ##### -->
 
 <!-- ##### jQuery (Necessary for All JavaScript Plugins) ##### -->
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,43 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: oshiro
- * Date: 29/09/18
- * Time: 18:45
- */
+<!-- Header Area Start -->
+<header class="header-area clearfix">
+    <!-- Close Icon -->
+    <div class="nav-close">
+        <i class="fa fa-close" aria-hidden="true"></i>
+    </div>
+    <!-- Logo -->
+    <div class="logo">
+        <a href="{{ route('home') }}"><img src="{{ asset('img/core-img/logo3.png') }}" alt=""></a>
+    </div>
+    <!-- Amado Nav -->
+    <nav class="amado-nav">
+        <ul>
+            <li class="active"><a href="{{ route('home') }}">Inicio</a></li>
+            <li><a href="#">Nosotros</a></li>
+            @auth
+                <li><a href="#">Mis servicios</a></li>
+                <li><a href="#">Mi perfil</a></li>
+                <li><a href="#">Salir</a></li>
+            @endauth
+        </ul>
+    </nav>
+    <!-- Button Group -->
+    <div class="amado-btn-group mt-30 mb-100">
+        @guest
+            <a href="{{ route('login') }}" class="btn amado-btn mb-15">Acceder</a>
+            <a href="#" class="btn amado-btn active">Publica tu servicio</a>
+        @endguest
+    </div>
+    <!-- Cart Menu -->
+    <div class="cart-fav-search mb-100">
+        <a href="cart.html" class="cart-nav"><img src="{{ asset('img/core-img/cart.png') }}" alt=""> Cart <span>(0)</span></a>
+        <a href="#" class="search-nav"><img src="{{ asset('img/core-img/search.png') }}" alt=""> Buscar</a>
+    </div>
+    <!-- Social Button -->
+    <div class="social-info d-flex justify-content-between">
+        <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+    </div>
+</header>
+<!-- Header Area End -->
