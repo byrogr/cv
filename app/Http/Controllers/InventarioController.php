8f527e584c9d1d7b48c1b73ef43e2f1d4f3cae32
Roger Rojas <roger.rojas.effio@gmail.com>
2018-09-30 15:37:27 -0500

    ososos

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -18,7 +18,7 @@ class InventarioController extends Controller
     public function list(Request $request, Departamento $departamento)
     {
         $order = $request->order ?: 'nombre';
-        $tours = Inventario::with(['servicios'], function($query) use ($departamento, $order) {
+        $tours = Inventario::with(['servicios'], function($query) use ($departamento) {
             $query->where('departamento_id', $departamento->id);
             //$query->orderBy($order, 'DESC');
         })->get();
